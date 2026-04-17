8bb65dc7cc8d8706d71df747d36dddf7b17648a6
Shruti Tilwar <shrutitilwar03@gmail.com>
2026-04-18 01:05:34 +0530

    first commit

--- Practy.py/Function.py
+++ Practy.py/Function.py
@@ -109,10 +109,9 @@
 
 #9. Write a Python program that accepts a hyphen-separated sequence of words as input and prints the words in a hyphen-separated sequence after sorting them alphabetically	
 # Sample Items : green-red-yellow-black-white
-# Expected Result : black-green-red-white-yellow
-
-        
+# Expected Result : black-green-red-white-
 
 
 
 
+a =10
